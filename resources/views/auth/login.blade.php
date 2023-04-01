bd39a96e5e16e871f686edd23db3e5b7f4e70710
driannns <saputraandrian064@gmail.com>
2023-04-01 12:30:16 +0700

    UI Login and forgot password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,12 @@
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
+    <div class="py-10 text-white text-center">
+
+        <h1 class="Poppins text-5xl font-bold" style="font-family:'Poppins', sans-serif;">Reservein.CO</h1>
+        <p class="PTserif text-3xl" style="font-family: 'PT Serif', serif;">Effortless Meeting At Your Finger Tips</p>
+    </div>
+    <form method="POST" action="{{ route('login') }}" class="w-7/12 mx-auto">
         @csrf
 
         <!-- Email Address -->
@@ -25,22 +30,26 @@
         </div>
 
         <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
+        <div class="flex items-center justify-between text-white mt-4">
+            <div>    
+                <label for="remember_me" class="inline-flex items-center">
+                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                    <span class="ml-2 text-sm">{{ __('Remember me') }}</span>
+                </label>
+            </div>
+            <div>    
+                @if (Route::has('password.request'))
+                <a class="underline text-sm hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
+                @endif
+            </div>
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
 
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
+            <x-primary-button class="ml-3 text-3xl" style="margin: auto; font-size: 20px ;padding: 20px 50px;">
+                {{ __('LOGIN') }}
             </x-primary-button>
         </div>
     </form>
